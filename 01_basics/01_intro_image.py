594f27da50ac4050b31ec0fd851f303f77362575
mojaviex <119891285+mojaviex@users.noreply.github.com>
2025-10-18 18:40:38 +0200

    Add files via upload

---
+++ 01_basics/01_intro_image.py
@@ -0,0 +1,13 @@
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import cv2
+
+print(cv2.__version__)
+
+image = cv2.imread(filename=r'01_basics/images/bear.jpg')
+
+cv2.imshow(winname='image', mat=image)
+cv2.waitKey(delay=0)
